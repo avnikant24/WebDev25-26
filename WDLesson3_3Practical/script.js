5cd617b366a1a1f2ec18f6195379ac99ffb89692
Avni Kant <avnik3@nycstudents.net>
2026-01-06 18:56:19 +0000

    3.3

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,21 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+function penguin(){
+    document.getElementById("highlight").innerHTML="Penguin";
+}
+
+function tiger(){
+    document.getElementById("highlight").innerHTML="Tiger";
+}
+
+function deer(){
+    document.getElementById("highlight").innerHTML="Deer";
+}
+
+
+function orangutans(){
+    document.getElementById("highlight").innerHTML="Orangutans";
+}
+
+function panda(){
+    document.getElementById("highlight").innerHTML="Panda";
+}
