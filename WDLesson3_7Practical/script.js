94994da566fb226b9fec7704757384b8b5fdc83e
Avni Kant <avnik3@nycstudents.net>
2026-01-27 18:54:59 +0000

    3.7

--- WDLesson3_7Practical/script.js
+++ WDLesson3_7Practical/script.js
@@ -3,12 +3,28 @@
       2) Perform the necessary calculations
       3) Display the results in the appropriate element
 */
-
+function calculateDistance(){
+      let x1 = parseFloat(document.getElementById("x1").value);
+      let y1 = parseFloat(document.getElementById("y1").value);
+      let x2 = parseFloat(document.getElementById("x2").value);
+      let y2 = parseFloat(document.getElementById("y2").value);
+      let output = document.getElementById("output");
+      let d = Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+      output.innerHTML = `Distance between points (${x1}, ${y1}) and (${x2}, ${y2}) is ${d}`;
+}
 /* Challenge 4: Create a function to serve as the event handler for the compound interest UI. Guidelines,
       1) Create variables and retrieve the information from the text inputs you created in Challenge 1.
       2) Perform the necessary calculations
       3) Display the results in the appropriate element
 */
 
-
+function calculateInterest(){
+      let P = parseFloat(document.getElementById("P").value);
+      let r = parseFloat(document.getElementById("r").value);
+      let n = parseFloat(document.getElementById("n").value);
+      let t = parseFloat(document.getElementById("t").value);
+      let output2 = document.getElementById("output2");
+      let A = P * Math.pow((1 + r / n), n * t); 
+      output2.innerHTML = `The amount after ${t} years is ${A}`;
+}
 
